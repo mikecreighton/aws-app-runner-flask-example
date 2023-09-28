45d51542c9d53d1239fbf8e6fb034aa4749abe05
Mike Creighton <mike@mikecreighton.com>
2023-09-28 14:24:12 -0700

    Adding a template to see if it works.

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ app = Flask(__name__, static_url_path='', static_folder='./static')
 
 @app.route('/')
 def home():
-    # return render_template('index.html')
-    return "Hello world"
+    return render_template('index.html', dynamic_variable="Hello World!")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
