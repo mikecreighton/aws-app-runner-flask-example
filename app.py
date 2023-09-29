3db8203a8659707eba984fa31735cf5bf7dfa550
Mike Creighton <mike@mikecreighton.com>
2023-09-29 10:23:53 -0700

    Removing any openAI calls. Just setting the API key to see if it's _any_ interaction with the SDK that's causing the problem.

--- app.py
+++ app.py
@@ -14,25 +14,25 @@ app = Flask(__name__, static_url_path='', static_folder='./static')
 def home():
     return render_template('index.html', dynamic_variable="Hello world!")
 
-@app.route('/openai')
-def openai_test():
+# @app.route('/openai')
+# def openai_test():
 
-    completion = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
-            max_tokens=128,
-            messages=[
-                {
-                    "role": "system",
-                    "content": "You are a helpful AI assistant."
-                },
-                {
-                    "role":"user",
-                    "content": "How are you doing today?"
-                },
-            ],
-            temperature=0.9)
-    message_content = completion.choices[0].message.content
-    return jsonify({"message": message_content})
+#     completion = openai.ChatCompletion.create(
+#             model='gpt-3.5-turbo',
+#             max_tokens=128,
+#             messages=[
+#                 {
+#                     "role": "system",
+#                     "content": "You are a helpful AI assistant."
+#                 },
+#                 {
+#                     "role":"user",
+#                     "content": "How are you doing today?"
+#                 },
+#             ],
+#             temperature=0.9)
+#     message_content = completion.choices[0].message.content
+#     return jsonify({"message": message_content})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
