481c3baf3b30621b126fe4a1245620e0c93455f8
Mike Creighton <mike@mikecreighton.com>
2023-09-29 10:38:07 -0700

    Changed venv. Took out openai library call, but seeing if installing it still works.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify, render_template, Response
 load_dotenv()
 
 # initialize openai with my key
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__, static_url_path='', static_folder='./static')
 
