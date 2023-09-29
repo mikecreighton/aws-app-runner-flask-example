a15dec796b58492792a47e921a64122b98111744
Mike Creighton <mike@mikecreighton.com>
2023-09-29 09:29:27 -0700

    Trying to get the app to just run an index and see if anything borks when the OpenAI call happens elsewhere.

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ app = Flask(__name__, static_url_path='', static_folder='./static')
 
 @app.route('/')
 def home():
+    return render_template('index.html', dynamic_variable="Hello world!")
+
+@app.route('/openai')
+def openai_test():
+
     completion = openai.ChatCompletion.create(
             model='gpt-3.5-turbo',
             max_tokens=128,
@@ -27,7 +32,7 @@ def home():
             ],
             temperature=0.9)
     message_content = completion.choices[0].message.content
-    return render_template('index.html', dynamic_variable=message_content)
+    return jsonify({"message": message_content})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
