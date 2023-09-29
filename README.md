6d4f4817fd382295efac696c59b299a1245fef00
Mike Creighton <mike@mikecreighton.com>
2023-09-29 08:55:55 -0700

    Re-enabling openai to see if it breakts.

--- README.md
+++ README.md
@@ -6,5 +6,7 @@
 
 ## Second attempt in App Runner config
 
-1. Add gunicorn to requirements.
-2. Use this to run the app: `gunicorn --bind=0.0.0.0:8080 app:app`
+1. Add gunicorn to requirements.txt.
+2. The build command is `pip install -r requirements.txt`
+3. Use this as the start command in App Runner: `gunicorn --bind=0.0.0.0:8080 app:app`
+4. Make sure the port is set to 8080
--- app.py
+++ app.py
@@ -1,34 +1,33 @@
 import os
-# import openai
+import openai
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify, render_template, Response
 
 load_dotenv()
 
 # initialize openai with my key
-# openai.api_key = os.getenv("OPENAI_API_KEY")
-api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__, static_url_path='', static_folder='./static')
 
 @app.route('/')
 def home():
-    # completion = openai.ChatCompletion.create(
-    #         model='gpt-3.5-turbo',
-    #         max_tokens=128,
-    #         messages=[
-    #             {
-    #                 "role": "system",
-    #                 "content": "You are a helpful AI assistant."
-    #             },
-    #             {
-    #                 "role":"user",
-    #                 "content": "How are you doing today?"
-    #             },
-    #         ],
-    #         temperature=0.9)
-    # message_content = completion.choices[0].message.content
-    return render_template('index.html', dynamic_variable="Is this an OpenAI thing?")
+    completion = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            max_tokens=128,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful AI assistant."
+                },
+                {
+                    "role":"user",
+                    "content": "How are you doing today?"
+                },
+            ],
+            temperature=0.9)
+    message_content = completion.choices[0].message.content
+    return render_template('index.html', dynamic_variable=message_content)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
