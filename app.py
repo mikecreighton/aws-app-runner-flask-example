c2c370561159edacc39bfc79e5202a6f6a7d4c1f
Mike Creighton <mike@mikecreighton.com>
2023-10-01 21:26:10 -0700

    Just trying flask by itself and skipping gunicorn to see what happens.

--- app.py
+++ app.py
@@ -6,17 +6,31 @@ from flask import Flask, request, jsonify, render_template, Response
 load_dotenv()
 
 # initialize openai with my key
-# openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__, static_url_path='', static_folder='./static')
 
 @app.route('/')
 def home():
-    return render_template('index.html', dynamic_variable="Hello world!")
+    completion = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            max_tokens=128,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful AI assistant."
+                },
+                {
+                    "role":"user",
+                    "content": "How are you doing today?"
+                },
+            ],
+            temperature=0.9)
+    message_content = completion.choices[0].message.content
+    return render_template('index.html', dynamic_variable=message_content)
 
 # @app.route('/openai')
 # def openai_test():
-
 #     completion = openai.ChatCompletion.create(
 #             model='gpt-3.5-turbo',
 #             max_tokens=128,
