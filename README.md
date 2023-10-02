e10e168fdf95e022ec2b5cf1b2036f941c34e548
Mike Creighton <mike@mikecreighton.com>
2023-10-02 11:14:40 -0700

    Adding grequests. Taking out openai and using basic requests.

--- README.md
+++ README.md
@@ -2,11 +2,17 @@
 
 ## First attempt in App Runner config
 
-1. Run Flask directly: `flask run -h 0.0.0.0 -p 8080`
+1. Run Flask directly: `flask run -h 0.0.0.0 -p 8080`.
 
 ## Second attempt in App Runner config
 
 1. Add gunicorn to requirements.txt.
-2. The build command is `pip install -r requirements.txt`
-3. Use this as the start command in App Runner: `gunicorn --bind=0.0.0.0:8080 app:app`
-4. Make sure the port is set to 8080
+2. The build command is `pip install -r requirements.txt`.
+3. Use this as the start command in App Runner: `gunicorn --workers=4 --bind=0.0.0.0:8080 app:app`.
+4. Make sure the port is set to 8080.
+
+## Current findings at this point
+
+- As soon as we include the `openai` SDK, everything breaks when deploying App Runner.
+- Otherwise, both run commands work just fine.
+- We do want to push on gunicorn, however, since that's really meant for a production deployment.
--- app.py
+++ app.py
@@ -1,52 +1,55 @@
 import os
-import openai
+import pprint
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify, render_template, Response
+from flask import Flask, render_template
+import requests
+using_gunicorn = os.getenv('DEPLOYED_WITH_GUNICORN')
+using_gunicorn = using_gunicorn == 'True'
+
+if using_gunicorn:
+    import grequests
 
 load_dotenv()
 
 # initialize openai with my key
-openai.api_key = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__, static_url_path='', static_folder='./static')
 
 @app.route('/')
 def home():
-    completion = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
-            max_tokens=128,
-            messages=[
-                {
-                    "role": "system",
-                    "content": "You are a helpful AI assistant."
-                },
-                {
-                    "role":"user",
-                    "content": "How are you doing today?"
-                },
-            ],
-            temperature=0.9)
-    message_content = completion.choices[0].message.content
-    return render_template('index.html', dynamic_variable=message_content)
+    return render_template('index.html', dynamic_variable="Sup homies")
 
-# @app.route('/openai')
-# def openai_test():
-#     completion = openai.ChatCompletion.create(
-#             model='gpt-3.5-turbo',
-#             max_tokens=128,
-#             messages=[
-#                 {
-#                     "role": "system",
-#                     "content": "You are a helpful AI assistant."
-#                 },
-#                 {
-#                     "role":"user",
-#                     "content": "How are you doing today?"
-#                 },
-#             ],
-#             temperature=0.9)
-#     message_content = completion.choices[0].message.content
-#     return jsonify({"message": message_content})
+@app.route('/openai')
+def openai_test():
+    request_url = 'https://api.openai.com/v1/chat/completions'
+    request_data = {
+        'model': 'gpt-3.5-turbo',
+        'temperature': 0.7,
+        'max_tokens': 256,
+        'messages': [
+            {
+                'role': 'system',
+                'content': 'You are a helpful AI assistant who loves using emojis.',
+            },
+            {
+                'role': 'user',
+                'content': 'Hello, how are you today?'
+            }
+        ]
+    }
+    request_headers = {
+        'Authorization': 'Bearer ' + OPENAI_API_KEY,
+        'Content-Type': 'application/json'
+    }
+    if using_gunicorn:
+        req = grequests.post(request_url, json=request_data, headers=request_headers)
+        response = grequests.map([req])[0]
+    else:
+        response = requests.post(request_url, json=request_data, headers=request_headers)
+    json_response = response.json()
+    message_content = json_response['choices'][0]['message']['content']
+    return render_template('index.html', dynamic_variable=message_content)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
